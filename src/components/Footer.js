a954e60dc24db5cf6566fcb0da9fd1b7898a3672
pendemic <mikeomskinner@aol.com>
2021-09-15 13:52:45 -0500

    Created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,39 @@ const Footer = () => {
     return (
         <section class="hero footer-bg is-halfheight">
             <div className="columns is-mobile is-centered">
-            <div class="columns is-half">
-    <div class="hero-body">
-      <p class="subtitle">About us
-      </p>
+            <div class="column is-half hero-body has-text-right">
+    <div class="subtitle">
+      <div>
+          About Us
+      </div>
+      <div>
+          Shipping Policies
+      </div>
+      <div>
+          Contact Us
+      </div>
+      <div>
+          Accessibility
+      </div>
     </div>
   </div>
-  <div class="columns is-half">
-    <div class="hero-body">
-      <p class="subtitle">About us
-      </p>
+  <div class="column is-half hero-body">
+    <div class="subtitle mt-4">
+      <div>
+          Cactus Club is an online plant shop that fits all of your needs.
+      </div>
+      <div>
+          Telephone: (123)-456-7890
+      </div>
+      <div>
+          Email: Hello@cactusclub.com
+      </div>
+      <div>
+          Address: 123 Cactus Cove
+      </div>
+      <div>
+          Follow us on social media!
+      </div>
     </div>
   </div>
             </div>
